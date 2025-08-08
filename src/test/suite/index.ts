2b1f65eea45f229f9cd1eb37e64e359f0acc047f
Sávio Serra <serra.santos.s@gmail.com>
2025-08-08 05:14:05 -0300

    chore: update lockfile and fix tests (#29)

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -4,13 +4,12 @@ import * as glob from "glob";
 
 export function run(): Promise<void> {
   // Create the mocha test
-  const mocha = new Mocha({ ui: "tdd" });
-  mocha.useColors(true);
+  const mocha = new Mocha({ ui: "tdd", color: true });
 
   const testsRoot = path.resolve(__dirname, "..");
 
   return new Promise((c, e) => {
-    glob("**/**.test.js", { cwd: testsRoot }, (err, files) => {
+    glob("**/**.test.js", { cwd: testsRoot }, (err: Error | null, files: string[]) => {
       if (err) {
         return e(err);
       }
